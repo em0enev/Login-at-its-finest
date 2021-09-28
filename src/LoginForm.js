2d98f6f34f81570f68745dd629eac26d2893d686
Emil Enev <emil.enev@abv.bg>
2021-09-28 19:57:27 +0300

    feat: add LoginForm component and styles

---
+++ src/LoginForm.js
@@ -0,0 +1,16 @@
+import styles from "./LoginForm.module.css"
+
+function LoginForm() {
+    return (
+        
+        <form className={styles.form} onSubmit={() => {console.log(styles, "dsdsds")}}>
+            <label className={styles.label} for="name">Name</label>
+            <input className={styles.input} type="text" name="name"></input>
+            <label className={styles.label} for="password">Password</label>
+            <input className={styles.input} type="text" name="password"></input>
+            <button className={styles.submit} type="submit">Submit</button>
+        </form>
+    );
+}
+
+export default LoginForm;
